dcb2597b26678099f57f58a8823c38a01176765c
Tim Tennant <ttennant@gmail.com>
2013-07-08 19:10:12 +0100

    More level 3 support

--- src/main/scala/org/tbag/uritemplate/Expression.scala
+++ src/main/scala/org/tbag/uritemplate/Expression.scala
@@ -6,27 +6,42 @@ import java.net.URLEncoder
 class Expression(val expression: String) {
   //"{" [ operator ] variable-list "}"
   private val (operator, identifiers) = expression.charAt(0).toString match {
-    case op@("+" | "#") => (Some(op), expression.replace(op, ""))
+    case op@("+" | "#" | "." | "/" | ";") => (Some(op), expression.replace(op, ""))
     case _ => (None, expression)
   }
 
 
-  def expand(variables: Map[String, Any]) = {
-    applyOperator(identifiers.split(",").map(identifier => encode(variables.getOrElse(identifier, "").toString)))
+  def expand(implicit variables: Map[String, Any]) = {
+    applyOperator(identifiers.split(",").map(identifier => (identifier -> encode(variables.getOrElse(identifier, "").toString))))
   }
 
-  private def applyOperator(values: Seq[String]) = {
+  private def applyOperator(values: Seq[(String, String)]) = {
+    val valuesOnly = values.toMap.values
     operator match {
-      case Some("+") => reservedExpansion(mkString(values, "", ","))
-      case Some("#") => reservedExpansion(mkString(values,"#", ","))
-      case Some(".") => mkString(values, ".", ".")
-      case Some(op) => values.mkString(",")
-      case _ => values.mkString(",")
+      case Some("+") => reservedExpansion(mkString(valuesOnly, "", Some(",")))
+      case Some("#") => reservedExpansion(mkString(valuesOnly, "#", Some(",")))
+      case Some(".") => mkString(valuesOnly, ".")
+      case Some("/") => mkString(valuesOnly, "/")
+      case Some(";") => mkString(withIdentifiers(values), ";")
+      case Some(op) => valuesOnly.mkString(",")
+      case _ => valuesOnly.mkString(",")
     }
   }
 
-  private def mkString(values: Seq[String], prefix: String, separator: String) = {
-    s"$prefix${values.mkString(separator)}"
+  private def withIdentifiers(values: Seq[(String, String)]) = {
+    values.map(keyValuePair => {
+      val key = keyValuePair._1
+      val value = keyValuePair._2
+      val separator = value match {
+        case "" => ""
+        case _ => "="
+      }
+      key + separator + value
+    })
+  }
+
+  private def mkString(values: Iterable[String], prefix: String, separator: Option[String] = None) = {
+    s"$prefix${values.mkString(separator.getOrElse(prefix))}"
   }
 
 
--- src/test/scala/org/tbag/uritemplate/ExpressionTest.scala
+++ src/test/scala/org/tbag/uritemplate/ExpressionTest.scala
@@ -14,36 +14,36 @@ class ExpressionTest extends Specification {
   }
 
   "Level 1 examples" should {
-    val variables = Map(
+    implicit val variables = Map(
       "var" -> "value",
       "hello" -> "Hello World!")
     "{var} => 'value'" in {
-      "{var}".expand(variables) must be equalTo ("value")
+      "{var}".expand must be equalTo ("value")
     }
     "{hello} => 'Hello%20World%21'" in {
-      "{hello}".expand(variables) must be equalTo ("Hello%20World%21")
+      "{hello}".expand must be equalTo ("Hello%20World%21")
     }
   }
 
   "Level 2 examples" should {
-    val variables = Map(
+    implicit val variables = Map(
       "var" -> "value",
       "hello" -> "Hello World!",
       "path" -> "/foo/bar"
     )
     "+ Reserved string expansion" in {
-      "{+var}".expand(variables) must be equalTo ("value")
-      "{+hello}".expand(variables) must be equalTo ("Hello%20World!")
-      "{+path}".expand(variables) must be equalTo ("/foo/bar")
+      "{+var}".expand must be equalTo ("value")
+      "{+hello}".expand must be equalTo ("Hello%20World!")
+      "{+path}".expand must be equalTo ("/foo/bar")
     }
     "# Fragment expansion, crosshatch-prefixed " in {
-      "{#var}".expand(variables) must be equalTo ("#value")
-      "{#hello}".expand(variables) must be equalTo ("#Hello%20World!")
+      "{#var}".expand must be equalTo ("#value")
+      "{#hello}".expand must be equalTo ("#Hello%20World!")
     }
   }
 
   "Level 3 examples" should {
-    val variables = Map(
+    implicit val variables = Map(
       "var" -> "value",
       "hello" -> "Hello World!",
       "empty" -> "",
@@ -52,23 +52,31 @@ class ExpressionTest extends Specification {
       "y" -> "768"
     )
     "String expansion with multiple variables" in {
-      "{x,y}".expand(variables) must be equalTo ("1024,768")
-      "{x,hello,y}".expand(variables) must be equalTo ("1024,Hello%20World%21,768")
+      "{x,y}".expand must be equalTo ("1024,768")
+      "{x,hello,y}".expand must be equalTo ("1024,Hello%20World%21,768")
     }
     "Reserved expansion with multiple variables" in {
-      "{+x,hello,y}".expand(variables) must be equalTo ("1024,Hello%20World!,768")
-      "{+path,x}".expand(variables) must be equalTo ("/foo/bar,1024")
+      "{+x,hello,y}".expand must be equalTo ("1024,Hello%20World!,768")
+      "{+path,x}".expand must be equalTo ("/foo/bar,1024")
     }
     "Fragment expansion with multiple variables" in {
-      "{#x,hello,y}".expand(variables) must be equalTo ("#1024,Hello%20World!,768")
-      "{#path,x}".expand(variables) must be equalTo ("#/foo/bar,1024")
+      "{#x,hello,y}".expand must be equalTo ("#1024,Hello%20World!,768")
+      "{#path,x}".expand must be equalTo ("#/foo/bar,1024")
     }
     "Label expansion, dot-prefixed" in {
-//      "{.var}".expand(variables) must be equalTo ("value")
-      //        |     |    X{.x,y}               X.1024.768
+      "{.var}".expand must be equalTo (".value")
+      "{.x,y}".expand must be equalTo (".1024.768")
     }
-
+    "Path segments, slash-prefixed" in {
+      "{/var}".expand must be equalTo ("/value")
+      "{/var,x}".expand must be equalTo ("/value/1024")
+    }
+    "Path-style parameters, semicolon-prefixed" in {
+      "{;x,y}".expand must be equalTo (";x=1024;y=768")
+      "{;x,y,empty}".expand must be equalTo (";x=1024;y=768;empty")
+    }
   }
+
 
   implicit def toExpression(expression: String): Expression = Expression(expression)
 
